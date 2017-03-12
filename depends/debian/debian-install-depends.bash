818479b42cffd0b610744f2c6392a66839b40725
Alexander Alemayhu <alexander@alemayhu.com>
2017-03-12 11:00:32 +0100

    debian: sync dependencies with slides
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- depends/debian/debian-install-depends.bash
+++ depends/debian/debian-install-depends.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Debian dependencies
+
 apt-get update
 apt-get install -y wget build-essential unzip bc
-apt-get install -y libncurses5-dev rsync cpio 
+apt-get install -y libncurses5-dev rsync cpio
 apt-get install -y gcc-multilib python
